04bacde8023cc819f399583d9a049342c27ddd3f
Real-Septicake <septicake056@gmail.com>
2024-03-09 09:46:59 -0500

    Corrected attribution of fouls, script now creates an alliance-level score breakdown, along with result of updated script

--- scripts/TBAData.py
+++ scripts/TBAData.py
@@ -8,6 +8,13 @@ from Config import API_KEY, MATCH_KEY
 TBA_BASE_URL = 'https://www.thebluealliance.com/api/v3'
 HEADERS = {'X-TBA-Auth-Key': API_KEY}
 
+stage_scores = {
+    'None': 0,
+    'Parked': 1,
+    'OnStage': 3,
+    'Spotlit': 4
+}
+
 offstage_status = ['None', 'Parked']
 
 alliances = ['red', 'blue']
@@ -41,7 +48,10 @@ fouls: Callable[[dict, str, bool], str] = lambda match, alliance, tech: match['s
 red_card: Callable[[dict, str, str], bool] = lambda match, alliance, bot: 'frc'+bot in match['alliances'][alliance]['dq_team_keys']
 g424: Callable[[dict, str], bool] = lambda match, alliance: bool(match['score_breakdown'][alliance]['g424Penalty'])
 
+total: Callable[[dict, str], str] = lambda match, alliance: match['alliances'][alliance]['score']
+
 info_headers = ['Event', 'Match #', 'Alliance', 'Team #', 'Taxis', 'Final Status', 'Trap', 'Fouls Caused', 'Tech Fouls Caused', 'Red Card', 'Caused 424', 'Can Harmonize']
+score_headers = ['Event', 'Match #', 'Alliance', 'Total Score', 'Auto Total Score', 'Taxi Score', 'Auto Speaker Scores', 'Auto Amp Scores', 'Teleop Total Note Score', 'Teleop Unamped Speaker Score', 'Teleop Amped Speaker Score', 'Teleop Amp Score', 'Total Stage Score', 'Final Status Score', 'Trap Score', 'Harmony Score', 'Total Foul Score', 'Fouls Score', 'Tech Fouls Score']
 
 matches_req = requests.get(TBA_BASE_URL + f'/event/{MATCH_KEY}/matches', headers=HEADERS)
 matches = matches_req.json()
@@ -71,8 +81,8 @@ for i in range(1, len(match_nums)):
                     climb_pos[num] = None
                 row.append(finalStatus(matches[match_idx], alliance, bot))
                 row.append(int(trapStatus(matches[match_idx], alliance, bot)))
-                row.append(fouls(matches[match_idx], other_alliance(alliance), False))
-                row.append(fouls(matches[match_idx], other_alliance(alliance), True))
+                row.append(fouls(matches[match_idx], alliance, False))
+                row.append(fouls(matches[match_idx], alliance, True))
                 row.append(int(red_card(matches[match_idx], alliance, team_num(matches[match_idx], alliance, idx))))
                 row.append(int(g424(matches[match_idx], other_alliance(alliance))))
                 bots.append(row)
@@ -101,3 +111,40 @@ writer = csv.writer(file, lineterminator='\n')
 writer.writerows(newCSV)
 
 newCSV = []
+newCSV.append(score_headers)
+for i in range(1, len(match_nums)):
+    try:
+        match_idx = match_nums.index(i)
+        for alliance in alliances:
+            total_final_status: int = 0
+            total_trap: int = 0
+            for bot in robot_tags:
+                total_final_status += stage_scores[finalStatus(matches[match_idx], alliance, bot)]
+
+            row: list[str] = []
+            row.append(MATCH_KEY)
+            row.append(str(i))
+            row.append(alliance)
+            row.append(total(matches[match_idx], alliance))
+            row.append(matches[match_idx]['score_breakdown'][alliance]['autoPoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['autoLeavePoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['autoSpeakerNotePoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['autoAmpNotePoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['teleopTotalNotePoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['teleopSpeakerNotePoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['teleopSpeakerNoteAmplifiedPoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['teleopAmpNotePoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['endGameTotalStagePoints'])
+            row.append(str(total_final_status))
+            row.append(matches[match_idx]['score_breakdown'][alliance]['endGameNoteInTrapPoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['endGameHarmonyPoints'])
+            row.append(matches[match_idx]['score_breakdown'][alliance]['foulPoints'])
+            row.append(matches[match_idx]['score_breakdown'][other_alliance(alliance)]['foulCount'] * 2)
+            row.append(matches[match_idx]['score_breakdown'][other_alliance(alliance)]['techFoulCount'] * 5)
+            newCSV.append(row)
+    except(ValueError):
+        break
+
+file = open(sys.argv[1]+'/TBAAllianceScores.csv', 'w', encoding='utf-8')
+writer = csv.writer(file, lineterminator='\n')
+writer.writerows(newCSV)
